915e7cdd2db7483837c8a8963af8a28af6ba4a89
Ahmet Bozaci <ahmt9417@gmail.com>
2021-01-11 16:03:52 +0300

    Add select tutorial 2

--- zoo.sql
+++ zoo.sql
@@ -1,4 +1,4 @@
--- 0 SELECT basics
+-- SELECT basics-0
 
 SELECT population FROM world
 WHERE name = 'Germany';
@@ -9,7 +9,7 @@ WHERE name IN ('Sweden', 'Norway', 'Denmark');
 SELECT name, area FROM world
 WHERE area BETWEEN 200000 AND 250000;
 
--- 1 SELECT name
+-- SELECT name-1
 
 -- Name start with y-
 SELECT name FROM world
@@ -66,3 +66,71 @@ WHERE capital LIKE '%_City';
 -- 14
 
 -- 15
+
+
+-- SELECT from World-2
+
+-- 2
+SELECT name
+FROM world
+WHERE population > 200000000;
+
+-- 3
+SELECT name, GDP / population
+FROM world
+WHERE population > 200000000;
+
+-- 4
+SELECT name, population / 1000000
+FROM world
+WHERE continent = 'South America';
+
+-- 5
+SELECT name, population
+FROM world
+WHERE name IN ('France', 'Germany', 'Italy');
+
+-- 6
+SELECT name
+FROM world
+WHERE name LIKE ('%United%');
+
+-- 7
+SELECT name, population, area
+FROM world
+WHERE area > 3000000 OR population > 250000000;
+
+-- 8
+SELECT name, population, area
+FROM world
+WHERE area > 3000000 XOR population > 250000000;
+
+-- 9
+SELECT name, ROUND(population/1000000,2), ROUND(GDP/1000000000,2)
+FROM world
+WHERE continent = 'South America';
+
+-- 10
+SELECT name, ROUND(GDP/ population,-3)
+FROM world
+WHERE GDP > 1000000000000;
+
+-- 11
+SELECT name, capital
+FROM world
+WHERE LENGTH(name) = LENGTH(capital);
+
+-- 12
+SELECT name, capital
+FROM world
+WHERE LEFT(name,1) = LEFT(capital,1) AND name != capital;
+
+-- 13
+SELECT name
+FROM world
+WHERE name LIKE '%a%'
+AND name LIKE '%e%'
+AND name LIKE '%i%'
+AND name LIKE '%o%'
+AND name LIKE '%u%'
+AND name NOT LIKE '% %';
